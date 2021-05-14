cb7014b7582165911ef3586b78a62b4b6d3320e7
syoch <hacksyoch@gmail.com>
2021-05-15 08:06:06 +0900

    fix import wrapper

--- override/import.py
+++ override/import.py
@@ -1,16 +1,18 @@
 from .core import ctx
+from . import config
+from . import block
 
 
 def myImport(name, _globals=None, _locals=None, fromlist=(), level=0):
     basename = name.split(".")[0]
-    if basename in utilConf["module"]:
+    if basename in config.blocks["module"]:
         raise Exception(f"Module {basename} is blocked.")
     else:
         obj = ctx["backup"]["__import__"](
             name, _globals, _locals, fromlist, level)
 
-    if basename in utilConf["funcs"]:
-        for funcnames in utilConf["funcs"][basename]:
-            setattr(obj, funcnames, block(
+    if basename in config.blocks["funcs"]:
+        for funcnames in config.blocks["funcs"][basename]:
+            setattr(obj, funcnames, block.block(
                 basename+"."+funcnames+"()"))
     return obj
