922e0597e3c33ce48ef2149cc13093204f3c0cf5
syoch <hacksyoch@gmail.com>
2021-05-15 08:05:14 +0900

    fix open wrapper

--- override/open.py
+++ override/open.py
@@ -1,8 +1,10 @@
 import pathlib
+from . import config
+from .core import ctx
 
 
 def myOpen(path, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None):
     basename = pathlib.Path(path).name
-    if basename in utilConf["file"]:
+    if basename in config.blocks["file"]:
         raise Exception("can't open "+basename+".")
     return ctx["backup"]["open"](path, mode, buffering, encoding, errors, newline, closefd, opener)
