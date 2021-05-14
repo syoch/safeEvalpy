a8dd0c2ebf624b1e374ba744089d0939e0793556
syoch <hacksyoch@gmail.com>
2021-05-15 08:00:04 +0900

    format

--- override/core.py
+++ override/core.py
@@ -12,7 +12,8 @@ def apply():
     for funcname in config.blocks["builtinFuncs"]:
         org[funcname] = builtins[funcname]
         if config.blocks["builtinFuncs"][funcname]:
-            builtins[funcname] = locals(
-            )[config.blocks["builtinFuncs"][funcname]]
+            builtins[funcname] = locals()[
+                config.blocks["builtinFuncs"][funcname]
+            ]
         else:
             builtins[funcname] = block(funcname+"()")
