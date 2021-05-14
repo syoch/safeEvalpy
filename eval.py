2300f47ab9e018f103366e8212f68f0d605bbe8e
syoch <hacksyoch@gmail.com>
2021-05-14 22:19:34 +0900

    add module depend

--- eval.py
+++ eval.py
@@ -1,27 +1,33 @@
-buf = io.StringIO()
-bak_stdout = sys.stdout
-sys.stdout = buf
-org = {}
-for funcname in utilConf["builtinFuncs"]:
-    org[funcname] = __builtins__[funcname]
-    if utilConf["builtinFuncs"][funcname]:
-        __builtins__[funcname] = locals()[utilConf["builtinFuncs"][funcname]]
-    else:
-        __builtins__[funcname] = block(funcname+"()")
-try:
-    # check (ListComp attack)
-    ret = eval(
-        src,
-        {
-            "__builtins__": __builtins__
-        },
-        {
-            "buf": buf,
-        }
-    )
-except Exception as ex:
-    error = str(ex)
-for funcname in utilConf["builtinFuncs"]:
-    __builtins__[funcname] = org[funcname]
-sys.stdout = bak_stdout
-stdout = buf.getvalue()
+import io
+import sys
+
+
+def _eval(src):
+    buf = io.StringIO()
+    bak_stdout = sys.stdout
+    sys.stdout = buf
+    org = {}
+    for funcname in utilConf["builtinFuncs"]:
+        org[funcname] = __builtins__[funcname]
+        if utilConf["builtinFuncs"][funcname]:
+            __builtins__[funcname] = locals(
+            )[utilConf["builtinFuncs"][funcname]]
+        else:
+            __builtins__[funcname] = block(funcname+"()")
+    try:
+        # check (ListComp attack)
+        ret = eval(
+            src,
+            {
+                "__builtins__": __builtins__
+            },
+            {
+                "buf": buf,
+            }
+        )
+    except Exception as ex:
+        error = str(ex)
+    for funcname in utilConf["builtinFuncs"]:
+        __builtins__[funcname] = org[funcname]
+    sys.stdout = bak_stdout
+    stdout = buf.getvalue()
