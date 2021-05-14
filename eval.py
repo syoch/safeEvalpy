4e53ca120229663160c3144f9e77892a24d66e63
syoch <hacksyoch@gmail.com>
2021-05-14 23:49:21 +0900

    fix eval

--- eval.py
+++ eval.py
@@ -1,12 +1,16 @@
 import io
 import sys
 from . import config
+import builtins as __builtins__
 
 
 def _eval(src):
+    # overriding stdout
     buf = io.StringIO()
     bak_stdout = sys.stdout
     sys.stdout = buf
+
+    # override functions
     org = {}
     for funcname in config.blocks["builtinFuncs"]:
         org[funcname] = __builtins__[funcname]
@@ -15,6 +19,8 @@ def _eval(src):
             )[config.blocks["builtinFuncs"][funcname]]
         else:
             __builtins__[funcname] = block(funcname+"()")
+
+    # EXECUTING!!!
     try:
         # check (ListComp attack)
         ret = eval(
@@ -28,7 +34,11 @@ def _eval(src):
         )
     except Exception as ex:
         error = str(ex)
+
+    # restore functions
     for funcname in config.blocks["builtinFuncs"]:
         __builtins__[funcname] = org[funcname]
+    # restore stdout
     sys.stdout = bak_stdout
+    # get value
     stdout = buf.getvalue()
