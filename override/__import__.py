9199ed871e72379d7857347bd18ed8ec9d81442b
syoch <hacksyoch@gmail.com>
2021-05-15 18:47:32 +0900

    change import blocked module exception type

--- override/__import__.py
+++ override/__import__.py
@@ -6,7 +6,7 @@ from . import block
 def func(name, _globals=None, _locals=None, fromlist=(), level=0):
     basename = name.split(".")[0]
     if basename in blockedModules:
-        raise Exception(f"Module {basename} is blocked.")
+        raise block.Block(f"Module {basename} is blocked.")
     else:
         obj = ctx["backup"]["__import__"](
             name, _globals, _locals, fromlist, level)
