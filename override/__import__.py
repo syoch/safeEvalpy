2ac7cc38b1e5fa65393f0b3327fa7e55d0a38796
syoch <hacksyoch@gmail.com>
2021-05-15 18:41:40 +0900

    move variable

--- override/__import__.py
+++ override/__import__.py
@@ -2,17 +2,27 @@ from .core import ctx
 from . import config
 from . import block
 
+blockedFunctions = {
+    "sys": ["exit"],
+    "os": ["system", "fork", "_exit", "popen", "abort", "chdir", "fchdir", "getcwd", "open", "fdopen", ],
+    "_thread": ["exit", "exit_thread"],
+    "time": ["sleep"],
+    "io": ["open", "open_code", "FileIO"],
+    "_io": ["open", "open_code", "FileIO"],
+    "tokenize": ["open", "_builtin_open"]
+}
+
 
 def func(name, _globals=None, _locals=None, fromlist=(), level=0):
     basename = name.split(".")[0]
-    if basename in config.blocks["module"]:
+    if basename in config.blockedFunctions["module"]:
         raise Exception(f"Module {basename} is blocked.")
     else:
         obj = ctx["backup"]["__import__"](
             name, _globals, _locals, fromlist, level)
 
-    if basename in config.blocks["funcs"]:
-        for funcnames in config.blocks["funcs"][basename]:
+    if basename in blockedFunctions:
+        for funcnames in blockedFunctions[basename]:
             setattr(obj, funcnames, block.block(
                 basename+"."+funcnames+"()"))
     return obj
