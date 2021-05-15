517171fd4ad505d27357d36adb8d633837af0f8a
syoch <hacksyoch@gmail.com>
2021-05-15 18:35:48 +0900

    add ann to  eval.py

--- eval.py
+++ eval.py
@@ -3,7 +3,7 @@ from .override.core import apply, restore, ctx
 from .filter.listcomp import check as check_listcomp
 
 
-def _eval(src):
+def _eval(src: str):
     apply()
     try:
         # check ListComp Attack!
