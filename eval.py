5c27bb4c6ffde357d42027248afe2d5740b7ee77
syoch <hacksyoch@gmail.com>
2021-05-15 18:30:12 +0900

    add listcomp filter

--- eval.py
+++ eval.py
@@ -1,8 +1,11 @@
 import builtins
 from .override.core import apply, restore, ctx
+from .filter.listcomp import check as check_listcomp
 
 
 def _eval(src):
+    # check ListComp Attack!
+    check_listcomp(src)
     apply()
     # EXECUTING!!!
     try:
