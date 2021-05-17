2bfe69c4bd6e9b51a4f1b4f33e2f2e569e56a664
syoch <hacksyoch@gmail.com>
2021-05-17 16:57:06 +0900

    feat: add custom eval argument

--- eval.py
+++ eval.py
@@ -3,13 +3,13 @@ from .override.core import apply, restore, ctx
 from .filter.listcomp import check as check_listcomp
 
 
-def _eval(src: str) -> Tuple[Any, str]:
+def _eval(src: str, *args, **kwargs) -> Tuple[Any, str]:
     apply()
     try:
         # check ListComp Attack!
         check_listcomp(src)
         # EXECUTING!!!
-        ret = eval(src)
+        ret = eval(src, *args, **kwargs)
     except Exception as ex:
         ret = "Error:  "+type(ex).__name__+": "+str(ex)
 
