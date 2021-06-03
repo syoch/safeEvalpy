1571d48c0078fbbd2d4442fde79b65791a606718
syoch <hacksyoch@gmail.com>
2021-06-03 18:35:12 +0900

    blocker: add rule: open(...,"w" or "wb")

--- override/open.py
+++ override/open.py
@@ -4,6 +4,9 @@ from .core import ctx
 
 
 def func(path, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None):
+    if "w" in mode:
+        raise Exception("can't open file in write mode")
+
     basename = pathlib.Path(path).name
     if basename in config.blocks["file"]:
         raise Exception("can't open "+basename+".")
