de22771fed949bfdc70d2e19bb361c532fbe9b22
syoch <hacksyoch@gmail.com>
2021-06-03 20:00:51 +0900

    update: insiviable core(module)  but can import that

--- eval.py
+++ eval.py
@@ -1,5 +1,5 @@
+import importlib
 from typing import Any, Tuple
-from .override.core import apply, restore, ctx
 from .filter.listcomp import check as check_listcomp
 import timeout_decorator
 import traceback
@@ -9,23 +9,24 @@ import traceback
 def _eval(
     src: str, __globals=None, __locals=None
 ) -> Tuple[Any, str]:
+    core = importlib.import_module(".override.core", __package__)
 
     if not __globals:
         __globals = globals()
     if not __locals:
         __locals = locals()
 
-    apply()
+    core.apply()
     try:
         # check ListComp Attack!
         check_listcomp(src)
         # EXECUTING!!!
         ret = eval(src, __globals, __locals)
-        restore()
+        core.restore()
     except Exception as ex:
-        restore()
+        core.restore()
         ret = ''.join(traceback.TracebackException.from_exception(ex).format())
 
-    stdout = ctx["stdout"].getvalue()
+    stdout = core.ctx["stdout"].getvalue()
 
     return ret, stdout
