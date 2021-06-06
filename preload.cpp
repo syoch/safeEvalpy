ccb260dd456ac0913109d8f5a6a3c7ec8c6133e6
syoch <hacksyoch@gmail.com>
2021-06-06 20:25:37 +0900

    update: fix preload.cpp

--- preload.cpp
+++ preload.cpp
@@ -10,17 +10,18 @@ bool fork_enabled = true;
 // (controller) py=>open() C=>? C++=>?
 extern "C" int open64(const char *pathname, int flags, unsigned int mode)
 {
+  auto org = (int (*)(const char *, int, unsigned int))(dlsym((void *)(-1), "open64"));
   if (!strcmp(pathname, "%fb"))
   {
     fork_enabled = false;
-    return 0;
+    return org("/dev/null", flags, mode);
   }
   else if (!strcmp(pathname, "%fnb"))
   {
     fork_enabled = true;
-    return 0;
+    return org("/dev/null", flags, mode);
   }
-  return ((int (*)(const char *, int, unsigned int))(dlsym((void *)(-1), "open64")))(pathname, flags, mode);
+  return org(pathname, flags, mode);
 }
 
 long prev = 0;
