7ca7c76dc3f09e1da0796249709ce4142fb49561
syoch <hacksyoch@gmail.com>
2021-06-06 20:16:51 +0900

    update: implement fork/open64 preload

--- preload.cpp
+++ preload.cpp
@@ -1,5 +1,28 @@
 #include <stdio.h>
 #include <dlfcn.h>
+#include <string.h>
+
+// global variables
+bool fork_enabled = true;
+
+// overrides
+
+// (controller) py=>open() C=>? C++=>?
+extern "C" int open64(const char *pathname, int flags, unsigned int mode)
+{
+  if (!strcmp(pathname, "%fb"))
+  {
+    fork_enabled = false;
+    return 0;
+  }
+  else if (!strcmp(pathname, "%fnb"))
+  {
+    fork_enabled = true;
+    return 0;
+  }
+  return ((int (*)(const char *, int, unsigned int))(dlsym((void *)(-1), "open64")))(pathname, flags, mode);
+}
+
 long prev = 0;
 extern "C" int fork()
 {
@@ -9,9 +32,15 @@ extern "C" int fork()
   if (prev == 0)
     prev = clock();
 
-  // ((int (*)())dlsym(RTLD_NEXT, "fork"))();
-
-  fprintf(stdout, "fork is blocked(by preload) %ld \n", clock() - prev);
+  if (fork_enabled)
+  {
+    return ((int (*)())dlsym(RTLD_NEXT, "fork"))();
+  }
+  else
+  {
+    fprintf(stdout, "syscall::fork() is blocked %ld \n", clock() - prev);
+    return 0;
+  }
 
   prev = clock();
   return 0;
