e087098804171b5a7256379f2235caf7382ef072
syoch <hacksyoch@gmail.com>
2021-06-06 18:32:51 +0900

    update: add rule

--- override/config.py
+++ override/config.py
@@ -16,7 +16,7 @@ blocks = {
 }
 blockedFunctions = {
     "sys": ["exit"],
-    "os": ["system", "fork", "forkpty", "register_at_fork", "_exit", "kill", "killpg", "popen", "abort", "chdir", "fchdir", "getcwd", "open", "fdopen", ],
+    "os": ["system", "ofork", "forkpty", "register_at_fork", "_exit", "kill", "killpg", "popen", "abort", "chdir", "fchdir", "getcwd", "open", "fdopen", "mkdir"],
     "_thread": ["exit", "exit_thread"],
     "time": ["sleep"],
     "io": ["open", "open_code", "FileIO"],
