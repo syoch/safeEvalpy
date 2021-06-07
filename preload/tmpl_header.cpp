8ddb5d444e4ad2e53a2bd247d18648b23b6babfe
syoch <hacksyoch@gmail.com>
2021-06-07 20:16:18 +0900

    update: fix open64(tmpk)

--- preload/tmpl_header.cpp
+++ preload/tmpl_header.cpp
@@ -2,6 +2,8 @@
 #include <dlfcn.h>
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 // global variables
 bool fork_enabled = true;
@@ -9,18 +11,18 @@ bool fork_enabled = true;
 // overrides
 
 // (controller) py=>open() C=>? C++=>?
-extern "C" int open64(const char *pathname, int flags, unsigned int mode)
+extern "C" int open64(const char *pathname, int flags, ...)
 {
-  auto org = (int (*)(const char *, int, unsigned int))(dlsym((void *)(-1), "open64"));
+  auto org = (int (*)(const char *, int, ...))(dlsym((void *)(-1), "open64"));
   if (!strcmp(pathname, "%fb"))
   {
     fork_enabled = false;
-    return org("/dev/null", flags, mode);
+    return org("/dev/null", flags);
   }
   else if (!strcmp(pathname, "%fnb"))
   {
     fork_enabled = true;
-    return org("/dev/null", flags, mode);
+    return org("/dev/null", flags);
   }
-  return org(pathname, flags, mode);
+  return org(pathname, flags);
 }
