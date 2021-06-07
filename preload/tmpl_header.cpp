86a5da9fe291482a54a0071c34edb911429bd497
syoch <hacksyoch@gmail.com>
2021-06-08 00:39:05 +0900

    update: impl blocker

--- preload/tmpl_header.cpp
+++ preload/tmpl_header.cpp
@@ -4,10 +4,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 
 // global variables
 bool fork_enabled = true;
-
+char *blockedFname = nullptr;
 // overrides
 
 // (controller) py=>open() C=>? C++=>?
@@ -26,17 +27,57 @@ extern "C" int open64(const char *pathname, int flags, ...)
   }
   else if (!strncmp(pathname, "%bf ", 3))
   {
-    fprintf(stdout, "registered");
-    fork_enabled = true;
+    auto target = pathname + 4;
+    auto len = strlen(target);
+    if (blockedFname)
+      free(blockedFname);
+    blockedFname = (char *)malloc(len);
+    for (size_t i = 0; i < len; i++)
+    {
+      blockedFname[i] = target[i];
+    }
+
     return org("/dev/null", flags);
   }
 
   auto fd = org(pathname, flags);
+  if (blockedFname)
   { // check 'token'
     auto work = dup(fd);
-    auto token =
+    auto token = org(blockedFname, O_RDWR);
 
+    struct stat w;
+
+    fstat(work, &w);
+    auto work_size = w.st_size;
+    fstat(token, &w);
+    auto token_size = w.st_size;
+
+    if (work_size == token_size)
+    {
+      char work_buf, token_buf;
+      bool isnot = false;
+      for (size_t i = 0; i < work_size; i++)
+      {
+        read(work, &work_buf, 1);
+        read(token, &token_buf, 1);
+        if (work_buf != token_buf)
+        {
+          isnot = true;
+          break;
+        }
+      }
+
+      if (not isnot)
+      {
         close(work);
+        close(token);
+        return org("%%", flags);
+      }
+    }
+
+    close(work);
+    close(token);
   }
   return org(pathname, flags);
 }
