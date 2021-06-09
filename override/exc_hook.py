6993471f35058cf2d089f3be6c51f33ea1cc622a
syoch <hacksyoch@gmail.com>
2021-06-09 21:32:56 +0900

    update: change stream destination

--- override/exc_hook.py
+++ override/exc_hook.py
@@ -1,11 +1,17 @@
+from . import core
+
+
 def myRaise(exc_type, exc_value, tb):
-    print('Exception:')
+    dest = core.ctx["stdout"]
+
+    print('Exception:', file=dest)
 
     while tb:
         print(
             '  in %s:%d' %
-            (tb.tb_frame.f_code.co_name, tb.tb_lineno)
+            (tb.tb_frame.f_code.co_name, tb.tb_lineno),
+            file=dest
         )
         tb = tb.tb_next
 
-    print('  Detail: (%s) %s' % (exc_type.__name__, str(exc_value)))
+    print(f'  Detail: ({exc_type.__name__}) {exc_value}', file=dest)
