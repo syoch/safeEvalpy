583bbe304e8a9bd42c66e510064d0421006912a0
syoch <hacksyoch@gmail.com>
2022-03-21 14:43:22 +0900

    update: fix 1

--- override/patches/__init__.py
+++ override/patches/__init__.py
@@ -8,24 +8,19 @@ enabled_patches = False
 def apply():
     global enabled_patches
 
-    if enabled_patches:
-        raise Exception("already applied")
+    if not enabled_patches:
+        preload.apply()
+        static.apply()
+        dynamic.apply()
 
-    preload.apply()
-    static.apply()
-    dynamic.apply()
-
-    enabled_patches = True
+        enabled_patches = True
 
 
 def restore():
     global enabled_patches
 
-    if not enabled_patches:
-        raise Exception("not applied")
-
-    static.restore()
-    dynamic.restore()
-    preload.restore()
-
-    enabled_patches = False
+    if enabled_patches:
+        static.restore()
+        dynamic.restore()
+        preload.restore()
+        enabled_patches = False
