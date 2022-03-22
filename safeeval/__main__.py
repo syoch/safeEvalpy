37e65dcdfb2e8cbfd6a841c95c57a8874393f004
syoch <hacksyoch@gmail.com>
2022-03-22 13:15:19 +0900

    update: Added __main__.py

---
+++ safeeval/__main__.py
@@ -0,0 +1,14 @@
+from . import _eval
+
+print(".exit for exit")
+
+while True:
+    src = input('>>> ')
+
+    if src == ".exit":
+        break
+
+    ret, out = _eval.eval(src)
+    print(ret)
+    print("---")
+    print(out)
