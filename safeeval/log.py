36a499e6e9b9a29a3c06274a4923ae71acb5a418
syoch <hacksyoch@gmail.com>
2022-03-25 18:27:02 +0900

    update: Added log.Block

--- safeeval/log.py
+++ safeeval/log.py
@@ -1,5 +1,42 @@
+import io
+
+
+indent = 0
 _p = print
 
+stack = []
 
-def log(x):
-    _p(x)
+
+def format(*args, **kwargs):
+    buf = io.StringIO()
+    _p(*args, **kwargs, file=buf, end="")
+    return buf.getvalue()
+
+
+def log(*args, **kwargs):
+    global indent
+    _p("| "*indent+format(*args, **kwargs))
+
+
+def begin_block(*args, **kwargs):
+    global indent
+    log("-", *args, **kwargs)
+    indent += 1
+
+
+def end_block():
+    global indent
+    indent -= 1
+    log("\\")
+
+
+class Block:
+    def __init__(self, *msg):
+        self.msg = format(*msg)
+
+    def __enter__(self):
+        begin_block(self.msg)
+
+    def __exit__(self, *args):
+        end_block()
+        return False
--- safeeval/switcher/switcher.py
+++ safeeval/switcher/switcher.py
@@ -3,18 +3,27 @@ from ..preload.switcher import patcher as preload
 from ..static.switcher import patcher as static
 from .patcher import patcher
 
+from .. import log
+
 
 @patcher.apply
 def apply():
-    preload.do_apply()
-    static.do_apply()
-    dynamic.do_apply()
+    # begin_block("Safeeval", "All")
 
+    with log.Block("Safeeval", "All", "Apply"):
+        preload.do_apply()
+        static.do_apply()
+        log.log("Applying dynamic patch")
+        dynamic.do_apply()
     return None
 
 
 @patcher.restore
 def restore(_):
-    preload.do_restore()
-    static.do_restore()
-    dynamic.do_restore()
+    with log.Block("Safeeval", "All", "Restore"):
+        preload.do_restore()
+        static.do_restore()
+        log.log("Restoreing dynamic patch")
+        dynamic.do_restore()
+
+    # end_block()
